04587194615b54ea4b590187cd2a4f378e215976
HoTienQuoc <hotienquoc0429@gmail.com>
2025-09-05 13:18:44 +0700

    Add admin authentication configuration, update routes for admin login and auth, and seed admin data

---
+++ LaravelLearnTutorial/app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Plan;
+use App\Models\User;
+use App\Models\History;
+use App\Models\Product;
+use App\Models\Negative;
+use App\Models\Positive;
+use App\Models\Subscription;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthAdminRequest;
+use Illuminate\Validation\ValidationException;
+
+class AdminController extends Controller
+{
+    //Admin dashboard
+    public function index(){
+        $negatives = Negative::all();
+        $positives = Positive::all();
+        $products = Product::all();
+        $histories = History::all();
+        $users = User::a11();
+        $plans = Plan::all();
+        $subscriptions = Subscription::all();
+
+        return view('admin.dashboard')->with([
+            'Negatives' => $negatives,
+            'Positives'=>$positives,
+            'Products'=>$products,
+            'History'=>$histories,
+            'Users'=>$users,
+            'Plans'=>$plans,
+            'Subscriptions'=>$subscriptions,
+        ]);
+    }
+
+    public function login(){
+        if(auth()->guard('admin')->check()){
+            return redirect()->route('admin.index');
+        }
+        return view('admin.login');
+    }
+
+    /**
+     * Log in the admin function
+     */
+    public function auth(AuthAdminRequest $request){
+        if($request->validated()){
+            if(auth()->guard('admins')->attempt([
+                'email' => $request->email,
+                'password' => $request->password,
+            ])){
+                $request->session()->regenerate();
+                return redirect()->route('admin.index');
+            }
+            else{
+                throw ValidationException::withMessages([
+                    'email' => 'These credentials do not match our records.'
+                ]);
+            }
+        }
+    }
+
+    /**
+     * Logout the admin
+     */
+    public function logout(){
+
+    }
+}
--- LaravelLearnTutorial/routes/web.php
+++ LaravelLearnTutorial/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AdminController::class,'login'])->name('admin.login');
+
+Route::get('/admin/auth', [AdminController::class,'auth'])->name('admin.auth');
+
